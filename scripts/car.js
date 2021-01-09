8c331a42c886b9fbee452e4f052010f0f699a74b
Maxim <kewin.rrrr@gmail.com>
2021-01-09 23:23:29 +0300

    ...

--- scripts/car.js
+++ scripts/car.js
@@ -23,9 +23,9 @@ let Car = function (x = 100, y = 100) {
 
   this.render = (car) => {
     if (this.speed < 40 * this.pressUp) {
-      this.speed += 2 * this.pressUp
+      this.speed += 1 * this.pressUp
     } else if (this.speed > 40 * this.pressUp) {
-      this.speed -= 2
+      this.speed -= 1
     }
 
     this.rotation += 0.25 * this.rudder * this.speed
@@ -38,44 +38,6 @@ let Car = function (x = 100, y = 100) {
     car.style.transform = `rotate(${this.rotation}deg)`
   }
   
-
-  this.checkOut = (coordinates) => {
-    
-    let x0 = this.position.x, y0 = this.position.y
-
-    let res = true
-    coordinates.forEach((coor, index) => {
-      let x1 = coor[0], y1 = coor[1], x2, y2
-
-      if (index + 1 !== coordinates.length) {
-        x2 = coordinates[index + 1][0]
-        y2 = coordinates[index + 1][1]
-      } else {
-        x2 = coordinates[0][0]
-        y2 = coordinates[0][1]
-      }
-
-      let t = ((x0 - x1) * (x2 - x1) + (y0 - y1) * (y2 - y1)) / (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
-
-      if (t > 1) t = 1
-      if (t < 0) t = 0
-
-      let l = Math.sqrt(Math.pow(x1 - x0 + (x2 - x1) * t, 2) + Math.pow(y1 - y0 + (y2 - y1) * t, 2))
-
-      
-
-      if (t == 0) {
-        res = res && !(l < roadWidth / 2)
-      } else {
-        res = res && !(l < roadWidth / 2)
-      }
-    })
-
-    // console.log(!res); // !
-
-    return !res
-  }
-
   this.checkDistance = (coordinates, step = 1, angle = 0) => {
     let d = 0
  
@@ -109,13 +71,22 @@ let Car = function (x = 100, y = 100) {
 
       // point.style.top = this.position.y - 5  + 'px'
       // point.style.left = this.position.x - 5  + 'px'
-      const point = document.querySelector('.point')
+      const point = document.querySelectorAll('.point')
       if (!res) {
         d += step
       } else {
-        // point.style.top = y0 + 'px'
-        // point.style.left = x0 + 'px'
-        //d = Math.sqrt(Math.pow(this.position.x - x0, 2) + Math.pow(this.position.y - y0, 2))
+        if (angle == -30) {
+          point[0].style.top = y0 + 'px'
+          point[0].style.left = x0 + 'px'
+        } else if (angle == 30) {
+          point[1].style.top = y0 + 'px'
+          point[1].style.left = x0 + 'px'
+        } else {
+          point[2].style.top = y0 + 'px'
+          point[2].style.left = x0 + 'px'
+        }
+
+
         break
       }
     }
